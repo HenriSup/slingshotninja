5b1483e2c36a07054ede3a41ad68f7a035db1d30
henri journel <henri.journel@gmail.com>
2017-11-27 22:34:34 +0100

    gravity stuffs

--- assets/Behaviors (1)/BallBehavior (6)/script.ts
+++ assets/Behaviors (1)/BallBehavior (6)/script.ts
@@ -2,17 +2,32 @@ class BallBehavior extends Sup.Behavior {
   
   private debuggerActors:Sup.Actor[] = Sup.getActor("Debugger").getChildren();
   private debuggerTextRenderers:Sup.TextRenderer[];
-  private body:Sup.ArcadePhysics2D.Body = this.actor.arcadeBody2D;
+  private cannonBody:CANNON.Body = this.actor.cannonBody.body;
   
   awake() {
     this.debuggerTextRenderers = this.debuggerActors.map(actor=>actor.textRenderer);
   }
 
   update() {
-    let velocity = this.body.getVelocity();
-    this.debuggerTextRenderers[0].setText(velocity.x +" "+velocity.y);
-    velocity.y = velocity.y-0.1; 
-    this.body.setVelocity(velocity);
+    let velocity = this.cannonBody.velocity;
+    this.cannonBody.linearDamping = 0.5;
+    let pushForce = 500;
+    this.debuggerTextRenderers[0].setText(velocity.x +" "+(velocity.y.toFixed(2)));
+    
+    if(Sup.Input.wasKeyJustPressed('Z')){
+      velocity.y = velocity.y + pushForce;
+    }
+    if(Sup.Input.wasKeyJustPressed('Q')){
+      velocity.x = velocity.x - pushForce;
+    }
+    if(Sup.Input.wasKeyJustPressed('S')){
+      velocity.y = velocity.y - pushForce;
+    }
+    if(Sup.Input.wasKeyJustPressed('D')){
+      velocity.x = velocity.x + pushForce;
+    }
+    //this.body.setCustomGravity(0,-0.5);
+     this.cannonBody.velocity=new CANNON.Vec3(velocity.x,velocity.y,0);
   }
 }
 Sup.registerBehavior(BallBehavior);
